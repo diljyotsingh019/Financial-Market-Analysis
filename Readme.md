afe0de0a418c99ae50c937e43909afa4575e09e5
diljyotsingh019 <diljyotsingh019@gmail.com>
2024-07-15 10:58:31 -0400

    flask app updated

--- Readme.md
+++ Readme.md
@@ -4,7 +4,7 @@ Develop an advanced Retrieval-Augmented Generation (RAG) agent designed to assis
 
 
 
-### Linux Installation Steps
+### Linux Installation Steps (Ubuntu)
 1. Create a virtual environment
 ```sh
 virtualenv --python python3.10 rag-agent
@@ -19,12 +19,21 @@ source rag-agent/bin/activate
 ```sh
 pip install -r requirements.txt
 ```
+4. Create a `config.py` file inside src/utils directory and add the following api keys
+```sh
+ALPHA_API = "<YOUR ALPHA VANTAGE API KEY>"
+PINECONE_API = "<YOUR PINECONE API KEY>"
+OPENAI_API_KEY = "<YOUR OPENAI API KEY>"
+```
 
-4. Execute the python script and open the flask UI on a web browser (Hosted Locally) 
+5. Execute the python script and open the flask UI on a web browser (Hosted Locally) 
 ```sh
 python3 src/pipeline/implementation_pipeline.py
 ```
 
-
+## Currently working on:
+1. Logging and code documentation
+2. Containerization (Docker)
+3. Testing 
 
  
--- src/pipeline/implementation_pipeline.py
+++ src/pipeline/implementation_pipeline.py
@@ -21,40 +21,47 @@ def home():
 
 @app.route("/insights", methods = ["GET", "POST"])
 def agent():
-    callback_handler = handler()
-    finance = rag_finance(PINECONE_API, OPENAI_API_KEY)
-    agent = finance.agent(callback_handler)
-    agent_result = agent.invoke({"input": query})
-    if not callback_handler.log_actions:
-        return render_template("home.html")
-    else:      
-        ai = ChatOpenAI(model = "gpt-4o", temperature=0)
-        prompt = f"""
-        Based on the query provided below, give me the name of the stock (eg - AAPL).
-        Give me a single name of the stock in the json format.
-        Query - 
-        {query}
+    global query
+    if request.method == "POST":
+        query = request.form["askai"]
+        return redirect(url_for("agent"))
+    else:
+        try:
+            database = Database(PINECONE_API)
+            database.delete_records()
+        except:
+            pass 
+        callback_handler = handler()
+        finance = rag_finance(PINECONE_API, OPENAI_API_KEY)
+        agent = finance.agent(callback_handler)
+        agent_result = agent.invoke({"input": query})
+        if not callback_handler.log_actions:
+            return render_template("home.html")
+        else:      
+            ai = ChatOpenAI(model = "gpt-4o", temperature=0)
+            prompt = f"""
+            Based on the query provided below, give me the name of the stock (eg - AAPL).
+            Give me a single name of the stock in the json format.
+            Query - 
+            {query}
 
-        Output format - 
-        1. stock_name
+            Output format - 
+            1. stock_name
 
-        Don't explain anything just give me the json output.
-        """
-        result = ai.invoke(prompt)
-        result = json.loads(result.content[7:-3])
-        collection = data_collection(ALPHA_API)
-        actions = {"CompanyDividends": ["dividends.html",  collection.company_dividends(result["stock_name"])],
-            "GainersLosers": ["gainers_losers.html", collection.gainers_losers()], 
-            "CompanyOverview": ["overview.html", collection.overview(result["stock_name"])],
-            "MarketNews": ["market_news.html", ""]}
-        
-        data = actions[callback_handler.log_actions[-1]["Action"].tool][1]
-        return render_template(actions[callback_handler.log_actions[-1]["Action"].tool][0], data = data, response = agent_result["output"])
-        # if request.method == "POST":
-        #     query = request.form["askai"]
-        #     return redirect(url_for("agent"))
-        # else:
-        #     return render_template("home.html")
+            Don't explain anything just give me the json output.
+            """
+            result = ai.invoke(prompt)
+            result = json.loads(result.content[7:-3])
+            collection = data_collection(ALPHA_API)
+            actions = {"CompanyDividends": ["dividends.html",  collection.company_dividends(result["stock_name"])],
+                "GainersLosers": ["gainers_losers.html", collection.gainers_losers()], 
+                "CompanyOverview": ["overview.html", collection.overview(result["stock_name"])],
+                "MarketNews": ["market_news.html", ""]}
+            
+            data = actions[callback_handler.log_actions[-1]["Action"].tool][1]
+            return render_template(actions[callback_handler.log_actions[-1]["Action"].tool][0], data = data, response = agent_result["output"])
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
